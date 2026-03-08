ca6a9e2271d4d245a72175d6b224f79f71e71663
Rajudas575 <rajudas575@gmail.com>
2026-03-08 18:06:15 +0530

    Install pdfkit dependency

--- index.js
+++ index.js
@@ -18,8 +18,15 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", /vercel\.app$/],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
 );
 
